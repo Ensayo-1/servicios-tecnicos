c579948a076fdca0b0f0722deeb2c321282e3135
Ensayo-1 <yuri.marinp@cun.edu.co>
2026-01-09 15:38:52 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 let current = 0;
 const images = document.querySelectorAll(".gallery-img");
-
-// mostrar solo la primera
-images[current].style.opacity = 1;
-
-// solo rota las primeras 3 aunque haya más
 const maxImages = Math.min(images.length, 3);
+
+images[0].style.opacity = 1;
 
 setInterval(() => {
   images[current].style.opacity = 0;
   current = (current + 1) % maxImages;
   images[current].style.opacity = 1;
-}, 3500); // más corto y elegante
+}, 3000);
